5d620a20b28729bd0877bb2d8e75df332ac7c5d1
LClauss <Laurent.Clauss@gmail.com>
2018-01-02 17:01:28 +0100

    add google fonts, add burger menu package

--- src/index.js
+++ src/index.js
@@ -6,6 +6,61 @@ import classnames from "classnames";
 import Tiles from './tiles/tiles'
 import { ResponsiveContainer, Area, LineChart, ComposedChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
+import { slide as Menu } from 'react-burger-menu'
+
+var burgerstyles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
+
+class Example extends React.Component {
+  showSettings(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
+      </Menu>
+    );
+  }
+}
+
+
 const data = [
   { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
   { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
@@ -58,7 +113,9 @@ const CONTENT_MAP: Record<ViewId, string> = {
 };
 
 const RootApp = () => (
-  <ViewIdMosaic
+  <div id="outer-container" style={{ height: "100%", width: "100%" }}>
+    <Example style={{ burgerstyles}} pageWrapId={"mosaic"} outerContainerId={"outer-container"}  width={'280px'} />  
+  <ViewIdMosaic id="mosaic"
     renderTile={(id, path) => (
       <ViewIdMosaicWindow
         path={path}
@@ -78,6 +135,7 @@ const RootApp = () => (
       }
     }}
   />
+  </div>
 );
 
 
